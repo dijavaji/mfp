38d7a056d286af20124e33b89c1050745631f3ef
diego-mac <dijavaji@gmail.com>
2022-06-29 17:14:30 -0500

    cambio prueba

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/marketingApp';
 export default() =>{
   return (
     <div>
-      <h1>hola aqui</h1>
+      <h1>hola mundo en github</h1>
       <hr/>
       <MarketingApp/>
     </div>
