bf286bfe88bdddbae5dde226c387cccc6e5bbb5f
diego-mac <dijavaji@gmail.com>
2022-06-30 13:28:05 -0500

    correccion compilacion

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,9 +1,7 @@
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
-
-const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode:'production',
@@ -17,9 +15,9 @@ const prodConfig = {
                             exposes: {
                               './MarketingApp':'./src/bootstrap',
                             },
-                            shared : packageJson.dependencies,
-                            }),
+                            shared: packageJson.dependencies,
+    }),
   ],
 };
 
-module.exports = merge(commonConfig,prodConfig);
+module.exports = merge(commonConfig, prodConfig);
