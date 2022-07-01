188cf27708b1c93718bffd2c9b70990097840d8b
diego-mac <dijavaji@gmail.com>
2022-07-01 13:50:10 -0500

    classnames  aleatorios para produccion

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
 import MarketingApp from './components/marketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
+
 export default() =>{
   return (
-    <BrowserRouter>
-    <div>
-      <Header/>
-      <MarketingApp/>
-    </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName} >
+          <div>
+            <Header/>
+            <MarketingApp/>
+          </div>
+        </StylesProvider>
+      </BrowserRouter>
   );
 };
